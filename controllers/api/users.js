225545ee657720abcab30cf764b1c57119bae3d7
username <jcaputo66@aol.com>
2022-06-01 15:10:59 -0700

    do not check token

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -4,14 +4,8 @@ const User = require('../../models/user');
 
 module.exports = {
   create,
-  login,
-  checkToken
+  login
 };
-
-function checkToken(req, res) {
-  console.log(req.user);
-  res.json(req.exp);
-}
 
 async function login(req, res) {
   try {
