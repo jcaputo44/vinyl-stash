611ab6448bcdcd0d1a2a25e0b0ac2d70dc983261
username <jcaputo66@aol.com>
2022-06-06 10:35:44 -0700

    grid-Layout

--- src/pages/MyCollectionPage/MyCollectionPage.jsx
+++ src/pages/MyCollectionPage/MyCollectionPage.jsx
@@ -1,5 +1,31 @@
+import './MyCollectionPage.css';
+
 export default function MyCollectionPage() {
   return (
+    <>
     <h1>My Collection</h1>
+    <div className="ColGrid">
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px" /></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+      <div><img src="https://i.imgur.com/J3RGlSa.jpg" alt="darn" height="150px"/></div>
+    </div>
+    </>
   );
 }
